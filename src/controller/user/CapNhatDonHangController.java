7aae174dfbdb987ce5bc5c1d60c862a771fd9e81
An Nguyen <annguyen.htvn@gmail.com>
2019-11-12 23:54:40 +0700

    Create CapNhatDonHangCOntroller

---
+++ src/controller/user/CapNhatDonHangController.java
@@ -0,0 +1,19 @@
+package controller.user;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "CapNhatDonHang", urlPatterns = { "/CapNhatDonHang" })
+public class CapNhatDonHangController extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect("/SachKyAnh/view/user/view/capnhat_donhang.jsp");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
