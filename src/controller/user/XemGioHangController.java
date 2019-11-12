ef8739abe9a816c1e4e5c492d0129ab1e9e90ffa
An Nguyen <annguyen.htvn@gmail.com>
2019-11-12 23:56:53 +0700

    create XoaDonHangController

---
+++ src/controller/user/XemGioHangController.java
@@ -0,0 +1,22 @@
+package controller.user;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "XemGioHang", urlPatterns = { "/XemGioHang" })
+public class XemGioHangController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect("/SachKyAnh/view/user/view/gio_hang.jsp");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
