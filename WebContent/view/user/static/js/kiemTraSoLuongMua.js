69d3b077b4d6c26097d749fbd8d578dce8e87513
An Nguyen <annguyen.htvn@gmail.com>
2019-11-14 00:04:51 +0700

    update javascript function of check quantity of add cart

--- WebContent/view/user/static/js/kiemTraSoLuongMua.js
+++ WebContent/view/user/static/js/kiemTraSoLuongMua.js
@@ -1,15 +1,40 @@
-function ktSoLuongMua(){
+function ktSoLuongMua(soLuongDB, tenSach){
 	var hopLe = true;
 	var mangSoLuong  = document.getElementsByClassName("sl_mua");
 	
-	for(var i = 0; i < mangSoLuong.length; i++){
-		var soLuong = mangSoLuong[i].value;
-		//Số lượng mua phải là kiểu số, và lớn hơn 0
-		if( isNaN(soLuong) == true || soLuong <= 0){
-			hopLe = false;
-			alert('Số lượng mua phải là kiểu số, lớn hơn 0. Vui lòng kiểm tra lại !');
-		}
-	}
+	var soLuong = Number(mangSoLuong[0].value);
 	
-	return hopLe;
+	//Số lượng mua phải là kiểu số, và lớn hơn 0
+	if( isNaN(soLuong) == true || soLuong <= 0){
+		hopLe = false;
+		alert('Số lượng mua phải là kiểu số, lớn hơn 0. Vui lòng kiểm tra lại !');
+		return hopLe;
+	}
+
+	//Số lượng mua không được vượt quá số lượng trong DB
+	if(soLuong > soLuongDB){
+		hopLe = false;
+		alert('Thêm thất bại. Sách \"' + tenSach+ '\" chỉ còn ' + soLuongDB + ' sản phẩm !');
+		return hopLe;
+	}
 }
+
+//function ktSoLuongMua(soLuongDB, tenSach){
+//	var hopLe = true;
+//	var mangSoLuong  = document.getElementsByClassName("sl_mua");
+//	
+//	for(var i = 0; i < mangSoLuong.length; i++){
+//		var soLuong = mangSoLuong[i].value;
+//		//Số lượng mua phải là kiểu số, và lớn hơn 0
+//		if( isNaN(soLuong) == true || soLuong <= 0){
+//			hopLe = false;
+//			alert('Số lượng mua phải là kiểu số, lớn hơn 0. Vui lòng kiểm tra lại !');
+//		}
+//		if(soLuong > soluongDB){
+//			hopLe = false;
+//			alert('Thêm thất bại. Sách \"' + tenSach+ '\" chỉ còn ' + soLuongDB + ' sản phẩm !')
+//		}
+//	}
+//	
+//	return hopLe;
+//}
