bff108074bda6ff214522ce1555d4b1d4d2feb2a
An Nguyen <annguyen.htvn@gmail.com>
2019-11-16 10:07:24 +0700

    update XoaGioHangController

--- src/controller/user/XoaGioHangController.java
+++ src/controller/user/XoaGioHangController.java
@@ -1,23 +1,50 @@
 package controller.user;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import java.util.Map;
 
 @WebServlet(name = "XoaGioHang", urlPatterns = { "/XoaGioHang" })
 public class XoaGioHangController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String MaSach = request.getParameter("MaSach");
+		
+		HttpSession session = request.getSession();
+		Map<String, Object> gioHang = (Map<String, Object>)session.getAttribute("GioHang");
+		if(gioHang != null ) {
+			if( MaSach != null && MaSach.equals("XoaTatCa") ) {
+				session.removeAttribute("GioHang");
+			}else {
+				Map<String, Object> danhSachChiTietGioHang = (Map<String, Object>)gioHang.get("DanhSachChiTietGioHang");
+				if( danhSachChiTietGioHang != null && MaSach != null) {
+					danhSachChiTietGioHang.remove(MaSach);
+					if( danhSachChiTietGioHang.size() == 0 ) {
+						session.removeAttribute("GioHang");
+					}
+				}
+			}
+			response.sendRedirect("/SachKyAnh/XemGioHang");
+		}
+		else {
+			PrintWriter writer = response.getWriter();
+			writer.print("<script type='text/javascript'>");
+			writer.print("alert('Giỏ hàng rỗng, vui lòng kiểm tra lại !');");
+			writer.print("location='userTrangChu';");
+			writer.print("</script>");
+		}
 	}
 
 }
