6fdd7a286d07bd9a65a3e8bf37964bed1619c8b2
An Nguyen <annguyen.htvn@gmail.com>
2019-11-20 23:10:19 +0700

    update XoaDonHangController

--- src/controller/user/XoaDonHangController.java
+++ src/controller/user/XoaDonHangController.java
@@ -10,13 +10,10 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "XoaDonHang", urlPatterns = { "/XoaDonHang" })
 public class XoaDonHangController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
 
 }
