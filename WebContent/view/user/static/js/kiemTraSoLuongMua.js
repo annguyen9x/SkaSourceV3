05d72c6ca3320ea37b54e62c621ff4911b425a51
An Nguyen <annguyen.htvn@gmail.com>
2019-11-20 23:23:18 +0700

    update kiemTraSoLuongMua in javascript

--- WebContent/view/user/static/js/kiemTraSoLuongMua.js
+++ WebContent/view/user/static/js/kiemTraSoLuongMua.js
@@ -17,24 +17,6 @@ function ktSoLuongMua(soLuongDB, tenSach){
 		alert('Thêm thất bại. Sách \"' + tenSach+ '\" chỉ còn ' + soLuongDB + ' sản phẩm !');
 		return hopLe;
 	}
+	
+	return hopLe;
 }
-
-//function ktSoLuongMua(soLuongDB, tenSach){
-//	var hopLe = true;
-//	var mangSoLuong  = document.getElementsByClassName("sl_mua");
-//	
-//	for(var i = 0; i < mangSoLuong.length; i++){
-//		var soLuong = mangSoLuong[i].value;
-//		//Số lượng mua phải là kiểu số, và lớn hơn 0
-//		if( isNaN(soLuong) == true || soLuong <= 0){
-//			hopLe = false;
-//			alert('Số lượng mua phải là kiểu số, lớn hơn 0. Vui lòng kiểm tra lại !');
-//		}
-//		if(soLuong > soluongDB){
-//			hopLe = false;
-//			alert('Thêm thất bại. Sách \"' + tenSach+ '\" chỉ còn ' + soLuongDB + ' sản phẩm !')
-//		}
-//	}
-//	
-//	return hopLe;
-//}
