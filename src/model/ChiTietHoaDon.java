34cefc8256908b07dd0e89e5c3ba226ce34d6390
An Nguyen <annguyen.htvn@gmail.com>
2019-11-20 22:53:19 +0700

    create ChiTietHoaDon replace ChiTietHoaDonBan in model

---
+++ src/model/ChiTietHoaDon.java
@@ -0,0 +1,45 @@
+package model;
+
+public class ChiTietHoaDon {
+	private int soHD;
+	private String maSach;
+	private int soLuong;
+	private float donGia;
+	
+	public ChiTietHoaDon() {
+		
+	}
+	
+	public ChiTietHoaDon(int soHD, String maSach, int soLuong, float donGia) {
+		super();
+		this.soHD = soHD;
+		this.maSach = maSach;
+		this.soLuong = soLuong;
+		this.donGia = donGia;
+	}
+	
+	public int getSoHD() {
+		return soHD;
+	}
+	public void setSoHD(int soHD) {
+		this.soHD = soHD;
+	}
+	public String getMaSach() {
+		return maSach;
+	}
+	public void setMaSach(String maSach) {
+		this.maSach = maSach;
+	}
+	public int getSoLuong() {
+		return soLuong;
+	}
+	public void setSoLuong(int soLuong) {
+		this.soLuong = soLuong;
+	}
+	public float getDonGia() {
+		return donGia;
+	}
+	public void setDonGia(float donGia) {
+		this.donGia = donGia;
+	}
+}
