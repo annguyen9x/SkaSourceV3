889c61d78cd69ee0502420fd16a8ec45ff1f49be
An Nguyen <annguyen.htvn@gmail.com>
2019-11-24 17:39:49 +0700

    update ThemSachVaoGioHangHangController in controller

--- src/controller/user/ThemSachVaoGioHangController.java
+++ src/controller/user/ThemSachVaoGioHangController.java
@@ -13,7 +13,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import dao.ChiTietHoaDonDao;
+import dao.HoaDonDao;
 import dao.SachDao;
+import model.ChiTietHoaDon;
+import model.HoaDon;
 import model.Sach;
 import util.KiemTraKieuSo;
 
@@ -27,28 +31,75 @@ public class ThemSachVaoGioHangController extends HttpServlet {
 		
 		String maSach = request.getParameter("MaSach").trim();
 		int soLuong = Integer.parseInt(request.getParameter("SoLuong").trim());
-		
+		SachDao sachDao = new SachDao();
 		HttpSession session = request.getSession();
-		Map<String, Object> gioHang = (Map)session.getAttribute("GioHang");//khachHang, dsChiTietGH
+		Map<String, Object> gioHang = (Map)session.getAttribute("GioHang");
 		
+		//Cập nhật Đơn Hàng
 		if( session.getAttribute("CapNhatDonHang") != null ) {
-			if(gioHang != null ) {
-				session.removeAttribute("GioHang");//Xóa giỏ hàng nếu đang mua hàng, tạo giỏ hàng mới
+			Map<String, Object> danhSachChiTietGioHang = (Map)gioHang.get("DanhSachChiTietGioHang");
+			Map<String, Object> chiTietGioHang = null;
+			
+			ChiTietHoaDonDao chiTietHoaDonDao = new ChiTietHoaDonDao();
+			HoaDonDao hoaDonDao = new HoaDonDao();
+			int soHD = ((HoaDon)gioHang.get("HoaDon")).getSoHD();
+			
+			if( danhSachChiTietGioHang.get(maSach) == null ) {
+				Sach sach = sachDao.getSachTheoMaSach(maSach);
+				
+				chiTietGioHang = new HashMap<String, Object>();
+				chiTietGioHang.put("TenSach", sach.getTenSach());
+				chiTietGioHang.put("UrlHinh", sach.getUrlHinh());
+				chiTietGioHang.put("DonGia", sach.getDonGia());
+				chiTietGioHang.put("SoLuongDB", sach.getSoLuong() - soLuong);
+				chiTietGioHang.put("TongSoLuongDB", sach.getSoLuong());
+				chiTietGioHang.put("SoLuong", soLuong);
+				
+				float tongTienMoi = hoaDonDao.getHoaDon(soHD).getTongTien() + soLuong*sach.getDonGia();
+				hoaDonDao.updateTongTien(tongTienMoi, soHD);
+				sachDao.updateSoLuong(sach.getSoLuong() - soLuong, maSach);
+				ChiTietHoaDon chiTietHoaDon = new ChiTietHoaDon(soHD, maSach, soLuong, sach.getDonGia());
+				chiTietHoaDonDao.insert(chiTietHoaDon);
+				
+				danhSachChiTietGioHang.put(maSach, chiTietGioHang);
 			}
-			session.removeAttribute("CapNhatDonHang");
-			//Đổ dữ liệu từ đơn hàng sang giỏ hàng, rồi cập nhật trên đơn hàng sau đó lưu database
-		}else {
+			else {
+				Sach sach = sachDao.getSachTheoMaSach(maSach);
+				
+				chiTietGioHang = (Map)danhSachChiTietGioHang.get(maSach);
+				int soLuongDBMoi;
+				int soLuongDBTrongGioHang = (int)chiTietGioHang.get("SoLuongDB");
+				if( soLuongDBTrongGioHang > sach.getSoLuong() ) {
+					soLuongDBMoi = sach.getSoLuong()-soLuong;
+				}else {
+					soLuongDBMoi = ((int)chiTietGioHang.get("SoLuongDB") - soLuong);
+				}
+				chiTietGioHang.put("SoLuongDB", soLuongDBMoi);
+				int soLuongMoi = (int)chiTietGioHang.get("SoLuong") + soLuong;
+				chiTietGioHang.put("SoLuong", soLuongMoi);
+				
+				float tongTienMoi = hoaDonDao.getHoaDon(soHD).getTongTien() + soLuong*sach.getDonGia();
+				hoaDonDao.updateTongTien(tongTienMoi, soHD);
+				sachDao.updateSoLuong(sach.getSoLuong() - soLuong, maSach);
+				ChiTietHoaDon chiTietHoaDon = new ChiTietHoaDon(soHD, maSach, (int)chiTietGioHang.get("SoLuong"), sach.getDonGia());
+				chiTietHoaDonDao.update(chiTietHoaDon);
+
+				danhSachChiTietGioHang.put(maSach, chiTietGioHang);
+			}
+			gioHang.put("DanhSachChiTietGioHang", danhSachChiTietGioHang);
+			session.setAttribute("GioHang", gioHang);
+		}
+		//Thêm sách vào giỏ hàng ( khi chưa tạo ĐH)
+		else 
+		{
 			Map<String, Object> GioHangMoi = null;
 			Map<String, Object> danhSachChiTietGioHang = null;
 			Map<String, Object> chiTietGioHang = null;
-			
-			SachDao sachDao = new SachDao();
 			
 			if( gioHang == null ) {
 				danhSachChiTietGioHang = new HashMap<String, Object>();
 				
 				Sach sach = sachDao.getSachTheoMaSach(maSach);
-				
 				chiTietGioHang = new HashMap<String, Object>();
 				chiTietGioHang.put("TenSach", sach.getTenSach());
 				chiTietGioHang.put("UrlHinh", sach.getUrlHinh());
@@ -65,8 +116,8 @@ public class ThemSachVaoGioHangController extends HttpServlet {
 			}else {
 				danhSachChiTietGioHang = (Map)gioHang.get("DanhSachChiTietGioHang");
 				if( danhSachChiTietGioHang.get(maSach) == null ) {
-					Sach sach = sachDao.getSachTheoMaSach(maSach);
 					
+					Sach sach = sachDao.getSachTheoMaSach(maSach);
 					chiTietGioHang = new HashMap<String, Object>();
 					chiTietGioHang.put("TenSach", sach.getTenSach());
 					chiTietGioHang.put("UrlHinh", sach.getUrlHinh());
@@ -80,7 +131,6 @@ public class ThemSachVaoGioHangController extends HttpServlet {
 				else {
 					
 					Sach sach = sachDao.getSachTheoMaSach(maSach);
-					
 					chiTietGioHang = (Map)danhSachChiTietGioHang.get(maSach);
 					int soLuongDBMoi;
 					int soLuongDBTrongGioHang = (int)chiTietGioHang.get("SoLuongDB");
@@ -113,21 +163,20 @@ public class ThemSachVaoGioHangController extends HttpServlet {
 					}
 				}
 			}
-			
-			//Phần chuyển trang
-			String url = request.getParameter("DuongDan");
-			if( url != null &&  url.equals("/view/user/view/chitiet_sp.jsp")) {
-				response.sendRedirect("/SachKyAnh/ChiTietSach?MaSach="+maSach);
-			}
-			else if( url != null && url.equals("/view/user/view/sp_cungloai.jsp")) {
-				String maLoaiSach = sachDao.getSachTheoMaSach(maSach).getMaLoaiSach();
-				response.sendRedirect("/SachKyAnh/SachTheoLoaiSach?MaLoaiSach="+maLoaiSach);
-			}
-			else {
-				response.sendRedirect("/SachKyAnh/userTrangChu");
-			}
 		}
 		
+		//Phần chuyển trang
+		String url = request.getParameter("DuongDan");
+		if( url != null &&  url.equals("/view/user/view/chitiet_sp.jsp")) {
+			response.sendRedirect("/SachKyAnh/ChiTietSach?MaSach="+maSach);
+		}
+		else if( url != null && url.equals("/view/user/view/sp_cungloai.jsp")) {
+			String maLoaiSach = sachDao.getSachTheoMaSach(maSach).getMaLoaiSach();
+			response.sendRedirect("/SachKyAnh/SachTheoLoaiSach?MaLoaiSach="+maLoaiSach);
+		}
+		else {
+			response.sendRedirect("/SachKyAnh/userTrangChu");
+		}
 	}
 
 }
