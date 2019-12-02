1e65366dee9439d8009ec53074e13380d0ac9132
An Nguyen <annguyen.htvn@gmail.com>
2019-12-02 21:43:59 +0700

    create KhoXuLyCapNhatTTDHController in Controller

---
+++ src/controller/admin/KhoXuLyCapNhatTTDHController.java
@@ -0,0 +1,79 @@
+package controller.admin;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.HoaDonDao;
+import model.NhanVien;
+import util.KiemTraNgayThang;
+
+@WebServlet(name = "KhoXuLyCapNhatTTDH", urlPatterns = { "/KhoXuLyCapNhatTTDH" })
+public class KhoXuLyCapNhatTTDHController extends HttpServlet {
+	HoaDonDao hoaDonDao = new HoaDonDao();
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		
+		HttpSession session = request.getSession();
+		if( session.getAttribute("DsDHCNTTDHloi") != null) {
+			session.removeAttribute("DsDHCNTTDHloi");
+		}
+		if( session.getAttribute("TrangThaiXuLyCapNhatTTDH") != null) {
+			session.removeAttribute("TrangThaiXuLyCapNhatTTDH");
+		}
+		
+		String[] dsSoHD = request.getParameterValues("SoHD");
+		
+		List DsDHCNTTDHloi = new ArrayList<>();
+		for(int i=0; i < dsSoHD.length; i++) {
+			int soHD = Integer.parseInt(dsSoHD[i]);
+			String tinhTrangDH =  request.getParameter("TinhTrangDH"+soHD);
+			
+			if( "1".equals(tinhTrangDH) ) {
+				tinhTrangDH = "Đang chuẩn bị hàng";
+			}
+			else if( "2".equals(tinhTrangDH)) {
+				tinhTrangDH = "Đợi người giao lấy hàng";
+			}
+			else if( "3".equals(tinhTrangDH)) {
+				tinhTrangDH = "Trả lại hàng về kho";
+			}
+			else if( "4".equals(tinhTrangDH)) {
+				tinhTrangDH = "Hoàn tất";
+			}
+			
+			
+			if(hoaDonDao.khoUpdateTinhTrangDonHang(soHD, tinhTrangDH) == false) {
+				DsDHCNTTDHloi.add(soHD);
+			}
+			session.setAttribute("TrangThaiXuLyCapNhatTTDH", "DaCapNhatTTDH");
+		}
+		
+		//update lại thông tin
+		if( session.getAttribute("DsDonHangVaKhachHang") != null) {
+			session.removeAttribute("DsDonHangVaKhachHang");
+		}
+		List<Object> dsDonHangVaKhachHang = hoaDonDao.dsDonHangVaKhachHang();
+		session.setAttribute("DsDonHangVaKhachHang", dsDonHangVaKhachHang);
+		
+		if(DsDHCNTTDHloi.size() > 0) {
+			session.setAttribute("DsDHCNTTDHloi", DsDHCNTTDHloi);
+		}
+		
+		response.sendRedirect("/SachKyAnh/view/admin/view/kho_cn_tinhtrang_dh.jsp");
+	}
+
+}
